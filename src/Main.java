e98b6260942e78373e885d71d18d55732acf73f4
YashwanthCH <chelloju1999yashwanth@gmail.com>
2024-08-21 21:22:29 +0530

    Initial commit with README and Main.java

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+
+1. Create a RESTful API to manage a list of products with the following details: id, name, and price. Add appropriate verfication points to test the response parameters.
+ 
+2. Create a RESTful API to manage a list of students with the following details: id, name, and grade. Add appropriate verfication points to test the response parameters.
+ 
