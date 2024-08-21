5477d90814533ae804fa5da87c356a21da100c79
YashwanthCH <chelloju1999yashwanth@gmail.com>
2024-08-21 21:38:09 +0530

    Add verify search feature

---
+++ src/VerifySearch.java
@@ -0,0 +1,5 @@
+ Create a HashSet of integers. Add at least ten integers to the set, including some duplicates. Iterate through the set using an Iterator and print each element.
+Check if a specific number is present in the set. Remove a number from the set and print the updated set.
+ 
+5. Create a HashMap to store student names (keys) and their scores (values).
+Add at least five entries to the map. Iterate through the map using the entrySet() method and print each key-value pair. Update the score of a specific student. Remove a student from the map and print the updated map.
